dca05b6fa040cbe43bb0da5e8c88980161ee8aa6
yiper <yiper.fan@gmail.com>
2019-08-14 15:48:06 +0800

    0.2.0 promise 版本

--- crawler.js
+++ crawler.js
@@ -5,44 +5,55 @@ const path = require('path');
 //const request = require('request');
 const cheerio = require('cheerio'); // 解析html的
 
-const url = 'http://wufazhuce.com/one/';
-let total = 0;
-let promiseAll = [];
+const host = 'http://wufazhuce.com/one/';
 
-async function getUrl(url) {
-    const data = await axios.get(url);
+async function getImgUrl(url) {
+    let response = null;
+    try {
+        response = await axios.get(url);
+    } catch (error) {
+        console.log(`error:${error}`);
+        return null;
+    }
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const src = $('.one-imagen>img').attr('src');
+    return src;
 }
 
-for (let index = 1100; index < 1110; index++) {
-    axios
-        .get(url + index)
-        .then(Response => {
-            const html = Response.data;
-            const $ = cheerio.load(html);
-            const url = $('.one-imagen>img').attr('src');
-            axios.get(url, { responseType: 'stream' }).then(response => {
-                // console.log(respone.data);
-                const rs = response.data;
-
-                if (!fs.existsSync('./oneimg')) {
-                    fs.mkdirSync('oneimg');
-                }
-                const ws = fs.createWriteStream(`./oneimg/${index}.jpg`);
-
-                try {
-                    rs.pipe(ws);
-                    total++;
-                    console.log(`${index}:success`);
-                } catch (error) {
-                    console.log(`${index}:fail , fail message: ${error}`);
-                }
-            });
-        })
-        .catch(data => {
-            console.log(`${index}:网页加载失败`);
-        });
+async function getImgStream(url) {
+    if (!url) {
+        return null;
+    }
+    const response = await axios.get(url, { responseType: 'stream' });
+    return response.data;
 }
 
-console.log(`end: 下载 ${total}`);
+async function main(start, end) {
+    let total = 0;
+    for (let index = start; index < end; index++) {
+        const url = await getImgUrl(host + index);
+        const stream = await getImgStream(url);
+
+        if (!fs.existsSync('./oneimg')) {
+            fs.mkdirSync('oneimg');
+        }
+        const ws = fs.createWriteStream(`./oneimg/${index}.jpg`);
+
+        try {
+            stream.pipe(ws);
+            total++;
+            console.log(`${index}:success`);
+        } catch (error) {
+            console.log(`${index}:fail , fail message: ${error}`);
+        }
+    }
+    return total;
+}
+
+main(1100, 1110).then(data => {
+    console.log(`end: 下载 ${data}`);
+});
 
 // 神奇的代码：readable.pipe(writable)
