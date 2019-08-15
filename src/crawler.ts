7d32fdc477231aeb04b4a0ad739e51cc17f0fbab
yiper <yiper.fan@gmail.com>
2019-08-15 15:44:10 +0800

    写法三,解决上个版本并发执行没法统计的问题

--- src/crawler.ts
+++ src/crawler.ts
@@ -39,63 +39,80 @@ async function main(start: number, end: number) {
     if (!fs.existsSync('./oneimg')) {
         fs.mkdirSync('oneimg');
     }
-    let promisesAll = [];
-    for (let index = start; index < end; index++) {
-        promisesAll.push(async () => {
-            const url = await getImgUrl(host + index);
-            const stream = await getImgStream(url);
-
-            if (!stream) {
-                console.log(`${index}:fail `);
-                return;
-            }
-
-            const ws = fs.createWriteStream(`./oneimg/${index}.jpg`);
-
-            try {
-                stream.pipe(ws);
-                total++;
-                console.log(`${index}:success`);
-            } catch (error) {
-                console.log(`${index}:fail , fail message: ${error}`);
-            }
-        });
+    let arr = [];
+    for (let index = start; index <= end; index++) {
+        arr.push(index);
     }
+
+    let promisesAll = arr.map(async index => {
+        const url = await getImgUrl(host + index);
+        const stream = await getImgStream(url);
+
+        if (!stream) {
+            console.log(`${index}:fail `);
+            return;
+        }
+
+        const ws = fs.createWriteStream(`./oneimg/${index}.jpg`);
+
+        try {
+            stream.pipe(ws);
+            total++;
+            console.log(`${index}:success`);
+        } catch (error) {
+            console.log(`${index}:fail , fail message: ${error}`);
+        }
+    });
     // console.log(promisesAll);
 
     // await Promise.all(promisesAll).then(() => total);
     for (let pro of promisesAll) {
-        pro();
+        await pro;
     }
 
-    // return total;
+    return total;
 }
 
-main(1500, 1520);
-// .then(data => {
-//     console.log(`end: 成功下载 ${data}`);
-// });
+main(1500, 1520).then(data => {
+    console.log(`end: 成功下载 ${data}`);
+});
 
+// 下面的是async学习内容
 async function aa() {
     const arr = [];
     let totla = 0;
-    for (let index = 0; index < 10; index++) {
-        arr.push(
-            new Promise((reslove, reject) => {
+
+    const promis = new Array(10).fill(1).map(async (element, index) => {
+        const i = await new Promise((reslove, reject) => {
+            setTimeout(() => {
                 console.log(index);
-                setTimeout(() => {
-                    console.log(index);
-                    totla++;
-                    reslove();
-                }, Math.random() * 10000);
-            })
-        );
-    }
-    return Promise.all(arr).then(() => {
-        return totla;
+                totla++;
+                reslove(index);
+            }, Math.random() * 10000);
+        });
+        return i;
     });
+
+    for (const pro of promis) {
+        console.log(pro);
+        await pro;
+    }
+
+    return totla;
 }
 
 //aa().then(data => console.log(`${data}:end`));
+
+const bb = async () => {
+    return 'aa';
+};
+const a = Promise.resolve(134);
+
+const c = async () => {
+    // const b = await bb ;
+    const b = await bb();
+    return b;
+};
+// c().then(console.log);
 
 // 神奇的代码：readable.pipe(writable)
